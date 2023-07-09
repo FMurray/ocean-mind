8e2040e2c5af4aedfefe5025861dfd84df90dfa4
Zachary Feldman <zachary.feldman@voloapps.com>
2023-07-09 16:53:26 -0400

    crawling

--- sls/lambdas/runRequestAI/src/crawl.ts
+++ sls/lambdas/runRequestAI/src/crawl.ts
@@ -1,53 +1,66 @@
-// import axios from 'axios';
-// import cheerio from 'cheerio';
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+export async function crawl(url: string): Promise<string> {
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+  let text = $('body').text();
+  // Replace anything within angled brackets, which should remove residual HTML tags
+  text = text.replace(/<[^>]*>/g, '');
+  // Replace multiple whitespaces, line breaks, and tabs with a single space
+  text = text.replace(/\s\s+/g, ' ');
+  // Trim leading and trailing spaces
+  text = text.trim();
+  return text;
+}
 
 // Define the initial URL to start crawling
-const initialUrl = 'https://noaa.gov';
-console.log(initialUrl)
+// const initialUrl = 'https://noaa.gov';
 
 // Set of visited URLs to avoid revisiting the same page
 // const visitedUrls: Set<string> = new Set();
 
-// // Function to crawl a URL
-// async function crawl(url: string): Promise<void> {
-//   // Avoid revisiting the same page
-//   if (visitedUrls.has(url)) {
-//     return;
-//   }
+// Function to crawl a URL and extract plain text
+// export async function crawl(url: string): Promise<string> {
+//   return new Promise<string>(async (resolve, reject) => {
+//     // Avoid revisiting the same page
+//     if (visitedUrls.has(url)) {
+//       resolve('');
+//       return;
+//     }
 
-//   // Mark the URL as visited
-//   visitedUrls.add(url);
+//     // Mark the URL as visited
+//     visitedUrls.add(url);
 
-//   try {
-//     const response = await axios.get(url);
-//     const html = response.data;
-//     const $ = cheerio.load(html);
+//     try {
+//       const response = await axios.get(url);
+//       const html = response.data;
+//       const $ = cheerio.load(html);
 
-//     // Extract the desired information from the page using Cheerio selectors
-//     const title = $('h1').text();
-//     console.log('Title:', title);
+//       // Extract plain text from the page
+//       const plainText = $('body').text();
 
-//     // Extract URLs from the page and crawl them recursively
-//     const links = $('a');
-//     links.each((index, element) => {
-//       const linkUrl = $(element).attr('href');
-//       if (linkUrl) {
-//         // Resolve relative URLs
-//         const absoluteUrl = new URL(linkUrl, url).href;
-//         // Recursively crawl the discovered URL
-//         crawl(absoluteUrl);
-//       }
-//     });
-//   } catch (error) {
-//     console.error('Error:', error);
-//   }
-// }
+//       // Extract URLs from the page and crawl them recursively
+//       const links = $('a');
+//       const crawlPromises: Promise<string>[] = [];
+//       links.each((index, element) => {
+//         const linkUrl = $(element).attr('href');
+//         if (linkUrl) {
+//           // Resolve relative URLs
+//           const absoluteUrl = new URL(linkUrl, url).href;
+//           // Recursively crawl the discovered URL and collect the promises
+//           crawlPromises.push(crawl(absoluteUrl));
+//         }
+//       });
 
-// // Start crawling from the initial URL
-// crawl(initialUrl)
-//   .then(() => {
-//     console.log('Crawling completed.');
-//   })
-//   .catch((error) => {
-//     console.error('Error:', error);
+//       // Wait for all the recursive crawl promises to resolve
+//       const results = await Promise.all(crawlPromises);
+
+//       // Concatenate all the plain text results and resolve the promise
+//       const allPlainText = [plainText, ...results].join(' ');
+//       resolve(allPlainText);
+//     } catch (error) {
+//       reject(error);
+//     }
 //   });
+// }
--- sls/lambdas/runRequestAI/src/index.ts
+++ sls/lambdas/runRequestAI/src/index.ts
@@ -1,9 +1,10 @@
 import * as dotenv from "dotenv";
-import { OpenAI  } from "langchain";
-// import { OpenAI, PromptTemplate, LLMChain } from "langchain";
+import { OpenAI } from "langchain";
+import { PromptTemplate, LLMChain } from "langchain";
 import { BufferMemory } from "langchain/memory";
 import { ConversationChain } from "langchain/chains";
 import { DynamoDBChatMessageHistory } from "langchain/stores/message/dynamodb";
+import { crawl } from "./crawl.ts";
 const allowOrigin = "https://ocean-mind.ai";
 
 dotenv.config();
@@ -47,19 +48,23 @@ export const handler: any = async (
   const day = date.getDate().toString().padStart(2, '0');
 
   const formattedDate = `${year}-${month}-${day}`;
+  console.log(formattedDate);
 
   const memory = new BufferMemory({
     chatHistory: new DynamoDBChatMessageHistory({
       tableName: "langchain",
       partitionKey: "id",
-      sessionId: formattedDate, // Or some other unique identifier for the conversation
+      sessionId: new Date().toISOString(),
+      // sessionId: formattedDate, // Or some other unique identifier for the conversation
       config: {
         region: "us-east-1"
       },
     }),
   });
 
-  // Template prompt chain
+  /**
+   * Template prompt chain
+   */
   // const template = "Tell me something about this agency {agency}?";
   // const prompt = new PromptTemplate({
   //   template: template,
@@ -69,19 +74,96 @@ export const handler: any = async (
   // const resChain = await chain.call({ agency });
   // console.log(resChain);
 
+  /**
+   * Call input from message 
+   */
+  // const chain = new ConversationChain({ llm: model, memory });
+  // const conversationChain = await chain.call({ input: message });
+  // console.log({ conversationChain });
+
+
+  const getSitesMessage = 'What are 3 different impactful environmental agency websites. Please return a valid json with an array called environmentalAgencies of name, urls, and descriptions';
   const chain = new ConversationChain({ llm: model, memory });
-  const conversationChain = await chain.call({ input: message });
-  console.log({ conversationChain });
+  const conversationChain = await chain.call({ input: getSitesMessage });
+  console.log(`conversation response ${conversationChain.response}`)
+
+  const parsedArray = parseArrayFromText(conversationChain.response);
+  console.log(parsedArray);
+
+  console.log('About to crawl this url', parsedArray[0].url)
+
+  // Start crawling from the initial URL
+  let plainText = ''
+  try {
+    // const plainText = await crawl(parsedArray[0].url);
+    plainText = await crawl(parsedArray[0].url);
+    console.log(`plainText: ${plainText}`)
+  } catch (err) {
+    console.log('error crawling...')
+    console.error(err);
+  }
+
+  const template = `From this site ${parsedArray[0].url} I've scraped this info \"\"{text}\"\". Please write 5 captivating tweets that include the url in the response`;
+  const prompt = new PromptTemplate({
+    template: template,
+    inputVariables: ["text"],
+  });
+
+  const chain2 = new LLMChain({ llm: model, prompt: prompt, memory: memory });
+  const tempChain = await chain2.call({ text: plainText });
+
+  console.log(tempChain);
 
   console.log('returning back')
-
   const response = {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": allowOrigin,
     },
-    body: JSON.stringify(conversationChain),
+    body: JSON.stringify(tempChain),
   };
 
   return response;
 };
+
+
+function parseArrayFromText(textBody: string) {
+  // Regular expression pattern to match the array within the text
+  const arrayPattern = /\[([\s\S]*?)\]/;
+
+  // Extract the array from the text using the regular expression
+  const arrayMatch = textBody.match(arrayPattern);
+
+  if (arrayMatch) {
+    // Get the matched array string
+    const arrayString = arrayMatch[0];
+    console.log(`arrayString ${arrayString}`)
+
+    // Check the type of arrayString
+    if (typeof arrayString === 'string') {
+      console.log('Is type string!')
+      const isValid = isValidJSON(arrayString);
+      console.log('Is Valid JSON:', isValid);
+      // Parse the array string into a JavaScript object
+      const parsedArray = JSON.parse(arrayString);
+
+      return parsedArray;
+    } else if (Array.isArray(arrayString)) {
+      console.log('Is type any array!')
+
+      // Return the array as is since it's already parsed
+      return arrayString;
+    }
+  } else {
+    return null; // Return null if no array is found in the text
+  }
+}
+
+function isValidJSON(jsonString: string) {
+  try {
+    JSON.parse(jsonString);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
